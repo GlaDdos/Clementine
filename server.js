004b85913d13c64fd5c09d082381c1e5e04e5c46
kamil_kawa <kamil.kawa@gmail.com>
2016-04-09 22:10:45 +0200

    Basic server and index file

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+
+var app = express();
+
+routes(app);
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000...");
+});
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/controllers'));
